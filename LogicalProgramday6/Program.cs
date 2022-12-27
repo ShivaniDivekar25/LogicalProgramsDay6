c365f8dfc0e412b469d0a43b7c36c926c8fd4aef
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-27 13:53:23 +0530

    Added Welcome to Logical Program

---
+++ LogicalProgramday6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgramday6
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs!");
+        }
+    }
+}
