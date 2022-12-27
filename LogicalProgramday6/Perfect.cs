8f620eee5b20b7625e13da6aa258cffed09fd151
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-27 14:35:33 +0530

    Added Perfect Number Program

---
+++ LogicalProgramday6/Perfect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDay6
+{
+    internal class Perfect      //Creating class
+    {
+        public static void PerfectNumber1(int number)       //Creating method
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            if (sum == number)
+                Console.WriteLine("{0} is perfect number", number);
+            else
+                Console.WriteLine("{0} is not perfect number", number);
+        }
+    }
+}
--- LogicalProgramday6/Program.cs
+++ LogicalProgramday6/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Logical Programs!");
             Console.WriteLine("please enter below option ");
-            Console.WriteLine("1:Fibonacci");
+            Console.WriteLine("1:Fibonacci\n2:PerfectNumber");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -14,6 +14,11 @@
                     Console.WriteLine("Please enter input value");
                     int count = Convert.ToInt32(Console.ReadLine());
                     Fibonacci.FibonacciNum(count);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter input value");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    Perfect.PerfectNumber1(number);
                     break;
                 default:
                     Console.WriteLine("Plaese choose correct option");
