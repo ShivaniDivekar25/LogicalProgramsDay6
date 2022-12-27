b4fad042052ad9c3c48a8a6f2732c477445a2c7a
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-27 14:47:21 +0530

    Added Prime Number program

---
+++ LogicalProgramday6/PrimeNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDay6
+{
+    internal class PrimeNumber
+    {
+        public static void PrimeNum(int input)
+        {
+            int count = 0;
+            for (int i = 1; i <= input; i++)
+            {
+                if (input % i != 0)
+                    count++;
+            }
+            if (count < 2)
+            {
+                Console.WriteLine("{0} is not prime number", input);
+            }
+            else
+            {
+                Console.WriteLine("{0} is prime number", input);
+            }
+
+        }
+    }
+}
--- LogicalProgramday6/Program.cs
+++ LogicalProgramday6/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Logical Programs!");
             Console.WriteLine("please enter below option ");
-            Console.WriteLine("1:Fibonacci\n2:PerfectNumber");
+            Console.WriteLine("1:Fibonacci\n2:PerfectNumber\n3:PrimeNumber");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -19,6 +19,11 @@
                     Console.WriteLine("Enter input value");
                     int number = Convert.ToInt32(Console.ReadLine());
                     Perfect.PerfectNumber1(number);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter input value");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    PrimeNumber.PrimeNum(input);
                     break;
                 default:
                     Console.WriteLine("Plaese choose correct option");
