f009004ccdc71180151e0230ba1603c005bf0689
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-27 22:43:33 +0530

    Added Decimal To Binary program

---
+++ LogicalProgramday6/DecimalToBinary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDay6
+{
+    internal class DecimalToBinary      //Creating class
+    {
+        public static void decimalToBinary()        //Creating method
+        {
+            int decVal;        //Initializing input values
+            int val;
+            string a = "";
+
+            Console.WriteLine("Enter the decimal value");       //Taking user input
+            decVal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Decimal: {0}", decVal);
+            while (decVal >= 1)     //while loop for required condition
+            {
+                val = decVal / 2;
+                a += (decVal % 2).ToString();
+                decVal = val;
+            }
+            string binValue = "";
+            for (int i = a.Length - 1; i >= 0; i--)     //For loop for required condition
+            {
+                binValue = binValue + a[i];
+            }
+            Console.WriteLine("Binary: {0}", binValue);
+        }
+    }
+}
